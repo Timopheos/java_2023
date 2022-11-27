29b1a12e8fc82c8c0eb548d0b15721709109cc06
Timopheos <Timopheos@hotmail.com>
2022-11-27 19:05:45 +0500

    commit massage

--- src/Lab_Work_1/Task_2.java
+++ src/Lab_Work_1/Task_2.java
@@ -1,7 +1,7 @@
 package Lab_Work_1;
 
 class task2{
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         int num; // в этой строке кода объявляется
 // переменная с именем num
