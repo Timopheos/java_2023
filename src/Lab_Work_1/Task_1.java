a678fd370414b103483ef20e98a14516367cd288
Timopheos <Timopheos@hotmail.com>
2022-11-27 18:40:47 +0500

    commit massage

---
+++ src/Lab_Work_1/Task_1.java
@@ -0,0 +1,10 @@
+package Lab_Work_1;
+
+public class Task_1 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/Lab_Work_1/Task_2.java
@@ -0,0 +1,16 @@
+package Lab_Work_1;
+
+public class Task_2 {
+    public static void main(String args[])
+    {
+        int num; // в этой строке кода объявляется
+// переменная с именем num
+        num = 100; // в этой строке кода переменной num
+// присваивается значение 100
+        System.out.println("num: " + num);
+        num = num * 2;
+        System.out.print("Znachenie num * 2 равно ");
+        System.out.println(num);
+    }
+
+}
