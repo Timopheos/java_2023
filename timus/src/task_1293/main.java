8ac839c158ce0852ea78a9bc0f4a493068666e67
Timopheos <Timopheos@hotmail.com>
2022-11-28 16:24:12 +0500

    commit massage

---
+++ timus/src/task_1293/main.java
@@ -0,0 +1,12 @@
+package task_1293;
+
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(((a*b)*n)*2);
+    }
+}
---
+++ timus/task_1293.java
@@ -0,0 +1,2 @@
+public class task_1293 {
+}
