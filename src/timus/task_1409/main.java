42ee3f68b4e1bca2179d40dcd78738e1db3f7d31
Timopheos <Timopheos@hotmail.com>
2022-12-04 16:21:41 +0500

    timus

---
+++ src/timus/task_1409/main.java
@@ -0,0 +1,13 @@
+package timus.task_1409;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(b-1);
+        System.out.println(a-1);
+    }
+}
