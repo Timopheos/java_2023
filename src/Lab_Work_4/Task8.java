d63aee69d5d0ac0e6fbad70caf78c0dca8feb06f
Timopheos <Timopheos@hotmail.com>
2022-12-29 21:41:40 +0500

    Lab_Work_4

--- src/Lab_Work_4/Task8.java
+++ src/Lab_Work_4/Task8.java
@@ -8,6 +8,7 @@ public class Task8 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        Scanner in2 = new Scanner(System.in);
 
         System.out.println("Введите текст для шифрования: ");
         String value = in.nextLine();
@@ -30,5 +31,25 @@ public class Task8 {
         System.out.println(Arrays.toString(chars));
 
 
+        System.out.println("Выполнить обратное преобразование? (y/n): ");
+
+        String que = in2.nextLine();
+
+        if (que.equals("y")) {
+            for (int i = 0; i < chars.length; i++) {
+                ints[i] = chars[i] - key;
+            }
+
+            for (int i = 0; i < chars.length; i++) {
+                chars[i] = (char) ints[i];
+
+            }
+            System.out.println(Arrays.toString(chars));
+        } else if (que.equals("n")) {
+            System.out.println("До свидания!");
+        } else System.out.println("Введите корректный ответ");
     }
+
+
 }
+
