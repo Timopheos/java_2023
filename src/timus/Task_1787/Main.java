d450f3f3089b04e20b72b6aef817e0f0c9afa33d
Timote <Himmash991@live.ru>
2023-01-18 22:05:59 +0500

    timus

---
+++ src/timus/Task_1787/Main.java
@@ -0,0 +1,26 @@
+package timus.Task_1787;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int k = in.nextInt();
+        int n = in.nextInt();
+
+        int left = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+
+            left = left + x - k;
+            if (left < 0) {
+                left = 0;
+            }
+        }
+        System.out.println(left);
+
+    }
+}
