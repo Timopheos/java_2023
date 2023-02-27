3b6e9d425ac2720c903f05d3f820a70c5353e9be
Timopheos <Timopheos@hotmail.com>
2023-02-27 22:38:01 +0500

    Lab_Work_8

---
+++ src/Lab_Work_8/Task_1/KlassFile1.java
@@ -0,0 +1,29 @@
+package Lab_Work_8.Task_1;
+
+public class KlassFile1 {
+
+    public static void main(String[] args) {
+        try {
+// Создание файла в текущей папке (где расположен файл KlassFile1.java)
+            File f1 = new File("MyFile1.txt");
+            f1.createNewFile();
+            if (f1.exists()) {
+
+                System.out.println("Создан!!!!");
+                System.out.println("Полный путь1: " + f1.getAbsolutePath());
+            }
+// Создание файла на диске Е и вывод полного пути
+            File f2 = new File("E:\\MyFile2.txt");
+            f2.createNewFile();
+            System.out.println("Полный путь 2: " + f2.getAbsolutePath());
+// Создание нескольких вложенных папок
+            File f3 = new File("E:\\Papka1\\Papka2\\Papka3");
+            f3.mkdirs();
+            System.out.println("Полный путь 3: " + f3.getAbsolutePath());
+        } catch (Exception e) {
+            System.out.println("Ошибка!!! " + e);
+        }
+    }
+}
+
+
