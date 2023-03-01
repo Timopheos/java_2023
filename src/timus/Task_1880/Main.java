e419fe126a4eb7e307d443b1659368a02da63294
Timopheos <Timopheos@hotmail.com>
2023-03-01 22:28:25 +0500

    Lab_Work_8

--- src/timus/Task_1880/Main.java
+++ src/timus/Task_1880/Main.java
@@ -1,8 +1,23 @@
 package timus.Task_1880;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String inputFileName ="src/timus/Task_1880/input.txt";
+        boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+        int marshallAndLilly = 2;
+
+
+
+            BufferedReader bufferedReader =
+                    oj ? new BufferedReader(new InputStreamReader(System.in)) :
+                            new BufferedReader(new FileReader(inputFileName));
 
 
     }
