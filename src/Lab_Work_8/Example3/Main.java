a6356b24e6c7ae40d200fc414fe8a26b844666f8
Timopheos <Timopheos@hotmail.com>
2023-03-03 20:02:56 +0500

    Lab_Work_8

---
+++ src/Lab_Work_8/Example3/Main.java
@@ -0,0 +1,44 @@
+package Lab_Work_8.Example3;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void readAllByArray(InputStream in) throws IOException {
+        byte[] buff = new byte[5];
+        while(true){
+            int count = in.read(buff);
+            if(count !=-1){
+                System.out.println("количество = " + count + ", buff = "
+                        + Arrays.toString(buff) + ", str = "
+                        +new String(buff,0,count,"cp1251"));
+            }else{
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "E:\\MyFile2.txt";
+        InputStream inFile = null;
+
+        try{
+            inFile = new FileInputStream(fileName);
+            readAllByArray(inFile);
+        }catch(IOException e){
+            System.out.println("Ошибка открытия - закрытия файла " + fileName  + e);
+        }finally {
+            if(inFile !=null){
+                try{
+                    inFile.close();
+                }catch (IOException ignore){
+
+                }
+            }
+        }
+
+    }
+}
