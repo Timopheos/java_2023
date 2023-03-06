e478e4289909b3c93ad6dd0adc394d8d04df6c50
Timopheos <Timopheos@hotmail.com>
2023-03-06 21:33:38 +0500

    Lab_Work_8

---
+++ src/Lab_Work_8/Example6/Main.java
@@ -0,0 +1,30 @@
+package Lab_Work_8.Example6;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Reader in = null;
+        Writer out = null;
+
+        try {
+            in = new FileReader("E:\\Read1.txt");
+            out = new FileWriter("E:\\Write1.txt", true);
+
+            int oneByte;
+                    while ((oneByte = in.read()) != -1) {
+                        out.write((char)oneByte);
+                        out.append((char)oneByte);
+                        System.out.print((char)oneByte);
+                    }
+        }catch(IOException e) {
+            System.out.println("Ошибка!! ");
+        }
+        finally{
+            in.close();
+            out.close();
+        }
+
+    }
+}
