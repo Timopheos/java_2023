3728c9b430cee0e0576b074d9b334bdf27968efa
Timopheos <Timopheos@hotmail.com>
2023-03-16 22:59:42 +0500

    timus

---
+++ src/timus/Task_1209/Main.java
@@ -0,0 +1,23 @@
+package timus.Task_1209;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        long n = in.nextLong();
+
+        for (long i = 0; i < n; i++) {
+            long x = in.nextLong() - 1;
+            long y = (long) Math.sqrt(8 * x + 1);
+
+            if (y * y == (8 * x + 1)) {
+                System.out.println("1 ");
+            } else {
+                System.out.println("0 ");
+            }
+        }
+    }
+}
